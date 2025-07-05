fd63e12fdbbbe01d8ea65406acb9a865dc56fb55
Aditya Shukla <Shadowshukla76@gmail.com>
2025-07-05 23:20:36 +0530

    Added main layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex flex-col py-5 px-6 min-h-screen w-full bg-gray-100 items-center justify-center ">
+    <main className="py-5 px-6 flex-1 w-full  ">
       <section className="flex-1 flex items-center max-w-6xl justify-center ">
 
       </section>
